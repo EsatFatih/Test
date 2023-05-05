d5d4b44c88229b70124318e2f22cce9a03bef7fc
EsatFatih <esata091@gmail.com>
2023-05-05 20:02:22 +0300

    4. commit

--- src/calismalar/Deneme.java
+++ src/calismalar/Deneme.java
@@ -2,78 +2,75 @@ package calismalar;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Deneme {
 
-    static Scanner scanner= new Scanner(System.in);
-
-    static int toplam = 0;
-
-    static int urun = 0;
-
-    static List<String>urunListesi = new ArrayList<>(Arrays.asList("Domates","Muz","Elma","Cilek","Erik"));
-
-    static List<Integer>fiyatListesi = new ArrayList<>(Arrays.asList(25,25,36,63,61));
-
+    // Bilgisayara karsi tas kagit makas oyunu oynayacağımız kodları yazınız
+    // Kural: 5 puana ilk ulasan oyunu kazanir
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Random rnd = new Random();
 
-       /*
-    2. SORU
+        int userPoimt = 0;
+        int computerPoint = 0;
 
-        Basit bir 5 ürünlü manav alisveris programi yaziniz.
+        do {
+            System.out.println("Lutfen secim yapiniz : \n1- Tas \n2- Kagit\n3- Makas");
+            int userChoice = scanner.nextInt();
 
-        1. Adim : urun listesinden urun sectir ve kac kilo oldugunu sor.
-        2. Adim : Baska bir urun almak isteyip istemedigini sor. istemiyorsa toplam miktari goster,
-                  istiyorsa tekrar urun sectir. Bu islemi alisverisi bitirmek isteyene kadar tekrarla.
-        3. Adim : Musteri her urun sectiginde, aldigi urunun fiyatini toplam fiyata ekle.
-        4. Adim : Alisveris bitince toplam odemesi gereken tutari goster.
-     */
+            int compChoice = rnd.nextInt(3) + 1;
 
-        System.out.println("***********Wise market************");
+            if (userChoice == 1 && compChoice == 2) {
+                System.out.println("Sizin seciminiz :" + userChoice + " Bilgisayarin secimi : " + compChoice);
+                System.out.println("");
+                System.out.println("Kagit tasi sarar. Uzgunuz puan alamadiniz");
+                computerPoint++;
 
-        urunFiyat();
-    }
+            } else if (userChoice == 1 && compChoice == 3) {
+                System.out.println("Sizin seciminiz :" + userChoice + " Bilgisayarin secimi : " + compChoice);
+                System.out.println("");
+                System.out.println("Tas Makasi kirar. Tebrikler 1 puan kazandiniz ");
+                userPoimt++;
 
-    public static int urunFiyat() {
+            } else if (userChoice == 2 && compChoice == 1) {
+                System.out.println("Sizin seciminiz :" + userChoice + " Bilgisayarin secimi : " + compChoice);
+                System.out.println("");
+                System.out.println("Kagit tasi sarar. Tebrikler 1 puan kazandiniz");
+                userPoimt++;
+            } else if (userChoice == 2 && compChoice == 3) {
+                System.out.println("Sizin seciminiz :" + userChoice + " Bilgisayarin secimi : " + compChoice);
+                System.out.println("");
+                System.out.println("Makas kagidi keser. Uzgunuz puan alamadiniz");
+                computerPoint++;
+            } else if (userChoice == 3 && compChoice == 1) {
+                System.out.println("Sizin seciminiz :" + userChoice + " Bilgisayarin secimi : " + compChoice);
+                System.out.println("");
+                System.out.println("Tas Makasi kirar. Uzgunuz puan alamadiniz");
+                computerPoint++;
+
+            } else if (userChoice == 3 && compChoice == 2) {
+                System.out.println("Sizin seciminiz :" + userChoice + " Bilgisayarin secimi : " + compChoice);
+                System.out.println("");
+                System.out.println("Makas kagidi keser. Tebrikler 1 puan kazandiniz");
+                computerPoint++;
+
+            } else {
+                System.out.println("Uzgunuz puan alamadınız");
+            }
 
 
-        for (int i = 1; i <=urunListesi.size() ; i++) {
-            System.out.println(i+" . urun"+ urunListesi.get(i-1)+"\t"+fiyatListesi.get(i-1));
+        } while (userPoimt != 5 && computerPoint != 5);
 
+        if (userPoimt > computerPoint) {
+            System.out.println("Tebrikler siz kazandiniz\n: Puaniniz : " + userPoimt + "  Bilgisayar Puani :" + computerPoint);
+        } else {
+            System.out.println("Uzgunuz kaybettiniz Puaniniz : " + userPoimt + " Bilgisayar Puani : " + computerPoint);
         }
-
-        System.out.println("Please choose product: ");
-        int urun = scanner.nextInt();
-
-        System.out.println("Please how much weight: ");
-        double kilo = scanner.nextDouble();
-
-
-
-
-
-        return urun;
-
     }
-
-
 }
-    
-
-
-
-
-
-
-
-
-
 
 
 
